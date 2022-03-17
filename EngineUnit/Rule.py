2f5f8bafa511be2b021da8b0f032e9b64c8e71fc
danieldanan1 <danieldanan1@gmail.com>
2022-03-17 14:55:28 +0200

    Added QuickGuide

--- EngineUnit/Rule.py
+++ EngineUnit/Rule.py
@@ -12,6 +12,7 @@ class SchemePath:
     def __init__(self, path:str):
         self.path = path
         self.__result =[]
+        self.__result_is_list = False
 
 
 
@@ -20,52 +21,43 @@ class SchemePath:
         self.__result = []
         path_items = self.path.split(".")
         self.__getPathRec(path_items, scheme, args)#TODO handle errors
-        return self.__result
+        if len(self.__result>0):
+            return self.__result if self.__result_is_list == True else self.__result[0]
+        return None
 
     def __getPathRec(self, path: list, scheme: dict, args:dict={}):
         while(len(path) > 0):
             if type(scheme) == list:
                 for sub_scheme in scheme:
                     sub_path = path.copy()
+                    self.__result_is_list = True
                     self.__getPathRec(sub_path,sub_scheme,args)
                 return
+
             item:str = path.pop(0)
-            if(item.startswith("#")):#var Defenishion
+            if(item.startswith("#")):#variable definition
                 var = item[1:]
                 for item in scheme.keys():
                     sub_path = path.copy()
                     sub_args = args.copy()
                     sub_args[var] = item
+                    self.__result_is_list = True
                     self.__getPathRec(sub_path,scheme[item],sub_args)
                 return
+
             if(item.startswith("$")):#var Value
                 item = args[item[1:]]#TODO raise if not found
+
             if(item in scheme):
                 scheme = scheme[item]#TODO test case when raise error if all paths not found
             else:
                 return
+
         if(type(scheme) == list):
             for item in scheme:
                 self.__result.append(PathData(item, args))
         else:
             self.__result.append(PathData(scheme, args))
-
-    # def getPathRec1(self,path: list, scheme: dict, args={}):
-    #     sub_path = path.copy()
-    #     if (len(path) == 0):
-    #         return
-    #     item = sub_path.pop(0)
-    #     if "{" in item:
-    #         var = item[1:-1]
-    #         for sub_item in scheme.keys():
-    #             item_args = args.copy()
-    #             item_args[var] = sub_item
-    #             self.getPathRec1(sub_path, scheme[sub_item], item_args)
-    #     else:
-    #         if item in scheme:
-    #             pass
-    #         else:
-    #             raise "path not exist"
 
 
 class Rule:
@@ -90,26 +82,21 @@ class Rule:
             Type.ALL : self.runAll
         }
 
-
         self.operator_functions = {
-            Operator.EXIST : lambda pathData: self.getPath(pathData) != [],
-            Operator.NOT_EXIST :lambda pathData: self.getPath(pathData)  == [],
-            Operator.LESS_THAN : lambda pathData : pathData.scheme>self.value,
-            Operator.GREATER_THAN : lambda pathData : pathData.scheme<self.value,
-            Operator.EQUAL :lambda pathData : pathData.scheme == self.value,
+            Operator.EXIST : lambda pathData: self.getPath(pathData) is not None,
+            Operator.NOT_EXIST :lambda pathData: self.getPath(pathData) is None,
+            Operator.LESS_THAN : lambda pathData : int(pathData.scheme) > int(self.value),
+            Operator.GREATER_THAN : lambda pathData : int(pathData.scheme) < int(self.value),
+            Operator.EQUAL :lambda pathData : int(pathData.scheme) == int(self.value),
             Operator.ANY : lambda pathData :self.value in pathData.scheme,
             Operator.CONTAIN : lambda pathData : self.value in self.getPath(pathData)
         }
 
-
     def __str__(self):
         return self.type.value
 
-
     def getPath(self, pathData:PathData):
-        schemePath = SchemePath(self.path)
-        results = schemePath.getPath(pathData.scheme, pathData.args)
-        return results[0] if len(results) == 1 else results
+        return SchemePath(self.path).getPath(pathData.scheme, pathData.args)
 
     def run(self,scheme:dict):
         self.__run(PathData(scheme, {}))
@@ -131,7 +118,6 @@ class Rule:
         if message is not None:
             print(is_success,self.type, self.path,pathData.args,message)
 
-
     def runBase(self,pathData:PathData):
         return self.operator_functions[self.operator](pathData)
 
@@ -141,8 +127,6 @@ class Rule:
             return self.rtrue.__run(pathData)
         if (not condition_result) and (self.rfalse is not None):
             return self.rfalse.__run(pathData)
-
-
 
     def runAnd(self,pathData:PathData):
         return all(r.__run(pathData) for r in self.rule)
@@ -157,6 +141,7 @@ class Rule:
     def runAll(self,pathData:PathData):
         pathsData:list[PathData] = self.getPath(pathData)
         return all(self.rule.__run(data) for data in pathsData)
+
     def typeNotExist(self,pathData:PathData):
         raise 'typeNotExist'
 
